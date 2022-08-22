23153ef515e57fb8062c32651fd8f54879fa504e
AidasD <ntaidas@gmail.com>
2022-08-22 21:01:47 +0100

    starting work

---
+++ main.js
@@ -0,0 +1,7 @@
+let container = document.getElementById('canvas')
+
+for (let i = 0; i < 16; i++) {
+    const box = document.createElement('div')
+    box.classList.add('box')
+    container.appendChild(box)  
+ }
