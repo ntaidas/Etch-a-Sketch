6b17075833f361396e04cc9b787b038b516d553e
AidasD <ntaidas@gmail.com>
2022-08-27 22:34:37 +0100

    v0.1 working but very limited build

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 let container = document.getElementById('canvas')
+const input = prompt('input grid size')
+let selection = input*input
+function color(){
+    box.style.backgroundColor = `black`
+}
 
-for (let i = 0; i < 16; i++) {
+
+for (let i = 0; i < selection; i++) {
     const box = document.createElement('div')
     box.classList.add('box')
-    container.appendChild(box)  
+    box.addEventListener('mouseover',()=> { box.style.backgroundColor = `black`})
+    container.appendChild(box)
+    container.style.gridTemplateColumns = `repeat(${input}, 1fr)`
  }
+
+ 
