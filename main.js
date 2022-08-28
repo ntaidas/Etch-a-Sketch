ef89f0e9fe7c65e75d6fb1d64af8d2305a8180fb
AidasD <ntaidas@gmail.com>
2022-08-28 21:19:31 +0100

    add buttons, moved grid selection to a button instead of autoload

--- main.js
+++ main.js
@@ -1,17 +1,34 @@
-let container = document.getElementById('canvas')
-const input = prompt('input grid size')
-let selection = input*input
-function color(){
-    box.style.backgroundColor = `black`
+const gridSize = document.getElementById('grid-size')
+const container = document.getElementById('canvas')
+const rainbow = document.getElementById('rainbow')
+let input = 0
+let rainbowMode = false
+function checkInput(){
+    
 }
 
+gridSize.addEventListener('click', ()=>{
+    container.innerHTML = ('')
+    input = prompt('input grid size')
+    if (input > 100 || input <2){
+        alert(`must be betwean 2 and 10 `)
+        return
+    }
+    let selection = input*input
+    for (let i = 0; i < selection; i++) {
+        const box = document.createElement('div')
+        box.classList.add('box')
+        box.addEventListener('mouseover',()=> { box.style.backgroundColor = `black`})
+        container.appendChild(box)
+        container.style.gridTemplateColumns = `repeat(${input}, 1fr)`
+     }
+})
 
-for (let i = 0; i < selection; i++) {
-    const box = document.createElement('div')
-    box.classList.add('box')
-    box.addEventListener('mouseover',()=> { box.style.backgroundColor = `black`})
-    container.appendChild(box)
-    container.style.gridTemplateColumns = `repeat(${input}, 1fr)`
- }
+
+
+
+
+
+
 
  
